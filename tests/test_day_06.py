ae7766f3532c873b9fc044f23122c5cedee56602
Thomas Plews <tplews@cisco.com>
2022-12-06 21:44:49 +0000

    Better tests

--- tests/test_day_06.py
+++ tests/test_day_06.py
@@ -12,24 +12,24 @@ from days import day_06
 
 
 @pytest.fixture
-def parsed_test_data() -> str:
+def parsed_test_data() -> list[str]:
     with open(os.path.join(REPO_ROOT, "data", "day_06_test.txt")) as f:
-        return f.read().strip()
+        return f.read().strip().splitlines()
 
 
-def test_part_1(parsed_test_data: str) -> None:
-    assert day_06.part_1(parsed_test_data) == 7
+def test_part_1(parsed_test_data: list[str]) -> None:
     assert day_06.part_1("abcd") == 4
-    assert day_06.part_1("bvwbjplbgvbhsrlpgdmjqwftvncz") == 5
-    assert day_06.part_1("nppdvjthqldpwncqszvftbrmjlhg") == 6
-    assert day_06.part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 10
-    assert day_06.part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 11
+    assert day_06.part_1(parsed_test_data[0]) == 7
+    assert day_06.part_1(parsed_test_data[1]) == 5
+    assert day_06.part_1(parsed_test_data[2]) == 6
+    assert day_06.part_1(parsed_test_data[3]) == 10
+    assert day_06.part_1(parsed_test_data[4]) == 11
 
 
-def test_part_2(parsed_test_data: str) -> None:
-    assert day_06.part_2(parsed_test_data) == 19
+def test_part_2(parsed_test_data: list[str]) -> None:
     assert day_06.part_2("abcdefghijklmn") == 14
-    assert day_06.part_2("bvwbjplbgvbhsrlpgdmjqwftvncz") == 23
-    assert day_06.part_2("nppdvjthqldpwncqszvftbrmjlhg") == 23
-    assert day_06.part_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg") == 29
-    assert day_06.part_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw") == 26
+    assert day_06.part_2(parsed_test_data[0]) == 19
+    assert day_06.part_2(parsed_test_data[1]) == 23
+    assert day_06.part_2(parsed_test_data[2]) == 23
+    assert day_06.part_2(parsed_test_data[3]) == 29
+    assert day_06.part_2(parsed_test_data[4]) == 26
